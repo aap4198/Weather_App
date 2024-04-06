a4b921f42560d9fc66235ec4749ae78a026d7a1e
Adnan Pathan <a.a-_-p4198@hotmail.com>
2024-04-06 17:04:03 +0530

    Rate limiter bug fix

--- src/app.js
+++ src/app.js
@@ -9,15 +9,15 @@ config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(router);
-app.use(errorController);
 app.use(
   rateLimit({
     windowMs: 60 * 1000,
-    max: 100,
+    max: 2,
     message: "Too many requests, please try again later.",
   })
 );
+app.use(router);
+app.use(errorController);
 app.get("*", function (req, res) {
   res.status(404).send("Route not found");
 });
